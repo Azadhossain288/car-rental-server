d3f48462ec7efeafd26560237e0414e17e30d294
Azadhossain288 <azadhossain016288@gmail.com>
2026-05-21 23:09:58 +0600

    setup express server and mongodb atlas connection

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config(); 
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Middleware 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Root API 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cxpbp1t.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    
+    await client.connect();
+    console.log(" Successfully connected to MongoDB Atlas!");
+
+  } catch (error) {
+    console.error(" MongoDB connection error:", error);
+  }
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
-    res.send('Car Rental Server is running successfully! ');
+  res.send('Car Rental Server is running perfectly!');
 });
 
 app.listen(port, () => {
-    console.log(`Server is running beautifully on port: ${port}`);
+  console.log(`🚗 Server is buzzing on port: ${port}`);
 });
